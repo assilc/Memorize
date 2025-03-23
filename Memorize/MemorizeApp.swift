44d80b34bffc27e09eb4eecb01402ff444efc0bc
Assil <assilc@gmail.com>
2025-03-23 17:36:55 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Assil Chehade on 23/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MemorizeTests/MemorizeTests.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeTests.swift
+//  MemorizeTests
+//
+//  Created by Assil Chehade on 23/03/2025.
+//
+
+import Testing
+@testable import Memorize
+
+struct MemorizeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
